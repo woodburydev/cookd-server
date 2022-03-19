6cdd2035145f0aecbfac41a641f3ea48bc043398
woodburydev <54697770+woodburydev@users.noreply.github.com>
2022-03-19 00:40:10 -0700

    Update typeorm.service.ts

--- src/shared/typeorm/typeorm.service.ts
+++ src/shared/typeorm/typeorm.service.ts
@@ -12,14 +12,18 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
 
     return {
       type: 'postgres',
-      url: process.env.DATABASE_URL,
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      database: process.env.DATABASE_NAME,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
       entities: ['dist/**/*.entity.{ts,js}'],
       migrations: ['dist/migrations/*.{ts,js}'],
       migrationsTableName: 'typeorm_migrations',
       logger: 'file',
       synchronize: false, // never use TRUE in production!
       ssl: {
-        rejectUnauthorized: true,
+        rejectUnauthorized: false,
       },
     };
   }
