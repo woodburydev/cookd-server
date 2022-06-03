eda08bc556bff00a8ef43a8b763f5f0631f06c71
woodburydev <54697770+woodburydev@users.noreply.github.com>
2022-06-03 13:08:38 -0700

    Update to Config

---
+++ database.configuration.ts
@@ -0,0 +1,45 @@
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+export class DatabaseConfiguration implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
+    if (process.env.NODE_ENV === 'PRODUCTION') {
+      return {
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: parseInt(process.env.DATABASE_PORT),
+        database: process.env.DATABASE_NAME,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        entities: ['dist/**/*.entity.{ts,js}'],
+        migrations: ['dist/src/migrations/*.{ts,js}'],
+        migrationsTableName: 'typeorm_migrations',
+        logger: 'file',
+        synchronize: true, // never use TRUE in production!
+        migrationsRun: false,
+        autoLoadEntities: true,
+        ssl: true,
+        extra: {
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        },
+      };
+    } else {
+      return {
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: parseInt(process.env.DATABASE_PORT),
+        database: process.env.DATABASE_NAME,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        entities: ['dist/**/*.entity.{ts,js}'],
+        migrations: ['dist/src/migrations/*.{ts,js}'],
+        migrationsTableName: 'typeorm_migrations',
+        autoLoadEntities: true,
+        logger: 'file',
+        synchronize: true, // never use TRUE in production!
+        migrationsRun: false,
+      };
+    }
+  }
+}
